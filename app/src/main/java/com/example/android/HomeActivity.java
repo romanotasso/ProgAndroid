a2f395ba0e350b7124450c24023f5908246c2b2d
romanotasso <romanofusco@hotmail.it>
2020-03-05 16:29:35 +0100

    Aggiunta tasto laterale

--- app/src/main/java/com/example/android/HomeActivity.java
+++ app/src/main/java/com/example/android/HomeActivity.java
@@ -1,18 +1,25 @@
 package com.example.android;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+
     DatabaseHelper db;
     Button mButtonEsplora;
     Button mButtonViaggi;
     Button mButtonCoupon;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,12 @@ public class HomeActivity extends AppCompatActivity {
         mButtonEsplora = findViewById(R.id.button_esplora);
         mButtonViaggi = findViewById(R.id.button_viaggi);
         mButtonCoupon = findViewById(R.id.button_coupon);
+        mDrawerLayout = findViewById(R.id.drawer);
+        mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
 
         mButtonEsplora.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,5 +58,17 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(couponIntent);
             }
         });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+
     }
 }
